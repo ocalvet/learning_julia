7aa934cb2728c9b66a8a325abcefabd0b908e69f
Ovidio R. Calvet <calveto@gmail.com>
2018-09-09 00:52:57 -0400

    Add a type

--- main.jl
+++ main.jl
@@ -2,4 +2,7 @@ include("utils.jl")
 
 a = Utils.add(9, 7)
 
-print("Hello Julia $a")
+b = Utils.Adder(4, 6)
+
+print("Hello Julia $a\n")
+print(b)
--- utils.jl
+++ utils.jl
@@ -3,4 +3,8 @@ export add
 function add(x,y)
   x + y
 end
+struct Adder
+  x::Int64
+  y::Int64
+end
 end
